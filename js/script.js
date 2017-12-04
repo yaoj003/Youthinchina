0b53611f1f8ca670073c3dbf479e506028b30ade
Jessie Yao <yaojessies@gmail.com>
2017-12-04 02:24:02 -0500

    fix

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
   //declare var arr for gif changing
-  var arr = ['img/gif1.gif','img/gif2.gif', 'img/gif3.gif' , 'img/gif4.gif'];
-  var counter = 0;
+  // var arr = ['img/gif1.gif','img/gif2.gif', 'img/gif3.gif' , 'img/gif4.gif'];
+  // var counter = 0;
 
   var scroll = $('html, body');
 
   setInterval(function(){
     var pos = scroll.scrollTop();
-    scroll.scrollTop(pos + 6);
+    scroll.scrollTop(pos + 7);
   }, 200)
 
 
@@ -31,29 +31,29 @@ $(document).ready(function() {
 
   // change logo gif
 
-  $('#imageContainer').click(function(){
-    if(counter <=arr.length-2){
-      counter +=1;
-    }else{
-      counter =0;
-    }
-
-    if(counter==3){
-      $('#imageContainer').css('width','180%');
-      $('#imageContainer').css('margin-left','-25%');
-      $('#imageContainer').css('margin-top','-15%');
-      $('#imageContainer').css('z-index','0');
-    }else{
-      $('#imageContainer').css('width','120%');
-      $('#imageContainer').css('margin-left','-7%');
-      $('#imageContainer').css('margin-top','5%');
-      $('#imageContainer').css('z-index','0');
-    }
-
-    console.log(counter);
-    $('#switch').attr('src', arr[counter]);
-
-  });
+  // $('#imageContainer').click(function(){
+  //   if(counter <=arr.length-2){
+  //     counter +=1;
+  //   }else{
+  //     counter =0;
+  //   }
+  //
+  //   if(counter==3){
+  //     $('#imageContainer').css('width','180%');
+  //     $('#imageContainer').css('margin-left','-25%');
+  //     $('#imageContainer').css('margin-top','-15%');
+  //     $('#imageContainer').css('z-index','0');
+  //   }else{
+  //     $('#imageContainer').css('width','120%');
+  //     $('#imageContainer').css('margin-left','-7%');
+  //     $('#imageContainer').css('margin-top','5%');
+  //     $('#imageContainer').css('z-index','0');
+  //   }
+  //
+  //   console.log(counter);
+  //   $('#switch').attr('src', arr[counter]);
+  //
+  // });
 
   // click to play/pause
   $('video').click(function(){
